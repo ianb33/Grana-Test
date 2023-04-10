1b1344a2f953cbb24197a180fff4637e26d5439a
ianb33 <ianberry33@icloud.com>
2023-04-10 13:08:58 -0400

    Auto stash before merge of "main" and "origin/main"

--- Assets/_Scripts/GameTimer.cs
+++ Assets/_Scripts/GameTimer.cs
@@ -22,7 +22,6 @@ public class GameTimer : MonoBehaviour
         timer.GetComponent<Image>().color = Color.green;
         duration = gameTime;
         StartCoroutine(UpdateColorCR());
-        
     }
 
     public IEnumerator UpdateColorCR()
@@ -58,34 +57,33 @@ public class GameTimer : MonoBehaviour
 
     private void OnTimerEnd()
     {
-        GameObject BackgroundBlur = Instantiate((GameObject)Resources.Load("Prefabs/BackgroundBlur"), new Vector3(0f, 0f, 0f), Quaternion.identity);
-        BackgroundBlur.transform.SetParent(GameObject.Find("GUICanvas").GetComponent<Transform>());
-        BackgroundBlur.transform.SetAsLastSibling();
+        /* GameObject BackgroundBlur = Instantiate((GameObject)Resources.Load("Prefabs/BackgroundBlur"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+         BackgroundBlur.transform.SetParent(GameObject.Find("GUICanvas").GetComponent<Transform>());
+         BackgroundBlur.transform.SetAsLastSibling();*/
 
         //show alert
         GameObject.Find("GameManager").GetComponent<GameManager>().DisplayAlert("endAlert", "Time's up!", 0.4f, 1f, 100, 2);
-    }
 
         AsyncOperation sceneLoadAsync = SceneManager.LoadSceneAsync("WinScreen", LoadSceneMode.Single);
 
+    }
 
-    }
-    private async void SendScoreData()
-    {
-        Debug.Log("Sending score data...");
-        BackendManager backendManager = this.AddComponent<BackendManager>();
-        
-        //Set parameters
-        int levelID = GameObject.Find("GameManager").GetComponent<GameManager>().levelID;
-        int score = GameObject.Find("GameManager").GetComponent<GameManager>().totalPoints;
-        
-        //Set up WWWForm with required fields
-        WWWForm SendScoreRequest = new WWWForm();
-        SendScoreRequest.AddField("levelID", levelID);
-        SendScoreRequest.AddField("score", score);
-        
-        string requestResult = await backendManager.POSTRequest("https://grana.vinniehat.com/api/score/submit", SendScoreRequest);
-        
-    }
-    
+    /* private async void SendScoreData()
+     {
+         Debug.Log("Sending score data...");
+         BackendManager backendManager = this.AddComponent<BackendManager>();
+
+         //Set parameters
+         int levelID = GameObject.Find("GameManager").GetComponent<GameManager>().levelID;
+         int score = GameObject.Find("GameManager").GetComponent<GameManager>().totalPoints;
+
+         //Set up WWWForm with required fields
+         WWWForm SendScoreRequest = new WWWForm();
+         SendScoreRequest.AddField("levelID", levelID);
+         SendScoreRequest.AddField("score", score);
+
+         string requestResult = await backendManager.POSTRequest("https://grana.vinniehat.com/api/score/submit", SendScoreRequest);
+
+     }*/
+
 }

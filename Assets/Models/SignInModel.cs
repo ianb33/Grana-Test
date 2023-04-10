5881ca4ceeee9698b4b20a926edd153878dd8a45
Emma <emmali.usa@gmail.com>
2023-04-10 11:40:56 -0400

    Updated signin script and models

---
+++ Assets/Models/SignInModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignInModel
+{
+    public string username { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+    
+    public string accessToken { get; set; }
+}
--- Assets/_Scripts/ResetPlayerPrefs.cs
+++ Assets/_Scripts/ResetPlayerPrefs.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -8,17 +11,21 @@ public class ResetPlayerPrefs : MonoBehaviour
 {
 
     [SerializeField] private BackendManager BackendManager;
+    
+    private string PlayerID;
+    
     // Start is called before the first frame update
     void Start()
     {
-        SignUp();
+        PlayerID  = SystemInfo.deviceUniqueIdentifier;
+        SignIn();
+        
     }
 
     // Logs the player into their device-specific account
     private async void SignIn()
     {
         //Set UUID for the specified device
-        string PlayerID = SystemInfo.deviceUniqueIdentifier;
         PlayerPrefs.SetString("PlayerID", PlayerID);
         
         //Format WWWForm to acquire access token
@@ -26,15 +33,22 @@ public class ResetPlayerPrefs : MonoBehaviour
         SigninRequest.AddField("username", PlayerID);
         SigninRequest.AddField("password", PlayerID);
 
-        string requestResult = await BackendManager.POSTRequest("https://grana.vinniehat.com/api/auth/signup", SigninRequest, false);
-        if(requestResult != null) PlayerPrefs.SetString("AccessToken", requestResult);
+        string requestResult = await BackendManager.POSTRequest("https://grana.vinniehat.com/api/auth/signin", SigninRequest, false);
+        if (requestResult != null)
+        {
+            var x = JsonConvert.DeserializeObject<SignInModel>(requestResult);
+            PlayerPrefs.SetString("accessToken", x.accessToken);
+            Debug.Log("accessToken: " + x.accessToken);
+        }
+        else SignUp();
         
+        Debug.Log("Completed SignIn request.");
+
     }
 
     private async void SignUp()
     {
         //Set UUID for the specified device
-        string PlayerID = "piss";
         PlayerPrefs.SetString("PlayerID", PlayerID);
         
         //Format WWWForm to input login info
@@ -45,7 +59,10 @@ public class ResetPlayerPrefs : MonoBehaviour
         
         //Send the form to the backend API
         string requestResult = await BackendManager.POSTRequest("https://grana.vinniehat.com/api/auth/signup", SignupRequest, false);
+
+        Debug.Log("Completed SignUp request.");
     }
     
 }
 
+//level ID and score

fea1c77ffb592875c5b006177c51cf12669611b1
Emma Li <emmali.usa@gmail.com>
2023-04-12 11:34:37 -0400

    Leaderboard connected to Backend

---
+++ Assets/Models/LeaderboardModel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardModel
+{
+    public int scoreID { get; set; }
+    public int levelID { get; set; }
+    public int score { get; set; }
+    public string createdAt { get; set; }
+    public string updatedAt { get; set; }
+    public int userID { get; set; }
+    public UserModel user { get; set; } 
+}
+
+public class UserModel
+{
+    public int userID { get; set; }
+    public string UUID { get; set; }
+    public string username { get; set; }
+}
--- Assets/_Scripts/LeaderboardTable.cs
+++ Assets/_Scripts/LeaderboardTable.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.VersionControl;
 using UnityEngine;
@@ -13,55 +14,56 @@ public class LeaderboardTable : MonoBehaviour
 {
     [SerializeField] private LeaderboardManager LeaderboardManager;
     [SerializeField] private GameObject LBCellPrefab;
-    
+
     //temporary, will be derived from current player information.
     [SerializeField] private int currentPlayerIndex;
 
     //List of players and their scores will be sorted before being pulled from the backend.
-    [SerializeField] private List<tempPlayer> playerList = new List<tempPlayer>
-    {
-        new tempPlayer{ playerName = "aaaaaaa", highScore = 1400},
-        new tempPlayer{ playerName = "bbbbbbb", highScore = 1300},
-        new tempPlayer{ playerName = "ccccccc", highScore = 1200},
-        new tempPlayer{ playerName = "ddddddd", highScore = 1100},
-        new tempPlayer{ playerName = "eeeeeee", highScore = 1000},
-        new tempPlayer{ playerName = "fffffff", highScore = 900},
-        new tempPlayer{ playerName = "ggggggg", highScore = 800},
-        new tempPlayer{ playerName = "hhhhhhh", highScore = 700},
-        new tempPlayer{ playerName = "iiiiiii", highScore = 600},
-        new tempPlayer{ playerName = "jjjjjjj", highScore = 500},
-        new tempPlayer{ playerName = "aaaaaaa", highScore = 400},
-        new tempPlayer{ playerName = "bbbbbbb", highScore = 300},
-        new tempPlayer{ playerName = "ccccccc", highScore = 200},
-        new tempPlayer{ playerName = "ddddddd", highScore = 100},
-        new tempPlayer{ playerName = "eeeeeee", highScore = 90},
-        new tempPlayer{ playerName = "fffffff", highScore = 80},
-        new tempPlayer{ playerName = "ggggggg", highScore = 70},
-        new tempPlayer{ playerName = "hhhhhhh", highScore = 60},
-        new tempPlayer{ playerName = "iiiiiii", highScore = 50},
-        new tempPlayer{ playerName = "jjjjjjj", highScore = 40},
-        new tempPlayer{ playerName = "eeeeeee", highScore = 90},
-        new tempPlayer{ playerName = "fffffff", highScore = 80},
-        new tempPlayer{ playerName = "ggggggg", highScore = 70},
-        new tempPlayer{ playerName = "hhhhhhh", highScore = 60},
-        new tempPlayer{ playerName = "iiiiiii", highScore = 50},
-        new tempPlayer{ playerName = "jjjjjjj", highScore = 40},
-    };
+    [SerializeField] private List<tempPlayer> playerList = new List<tempPlayer>();
 
     private void Start()
     {
-        GenerateList();
+        GetLeaderboard();
     }
 
+   
+    private async void GetLeaderboard()
+    {
+        //Format WWWForm to acquire score data
+        WWWForm ScoresRequest = new WWWForm();
+        ScoresRequest.AddField("levelID", LeaderboardManager.LevelID);
+
+        string requestResult = await BackendManager.GETRequest($"https://grana.vinniehat.com/api/score/scores/{LeaderboardManager.LevelID}");
+        if (requestResult != null)
+        {
+            //Deserialize score data and convert to List<tempPlayer>
+            var x = JsonConvert.DeserializeObject<List<LeaderboardModel>>(requestResult);
+            foreach (LeaderboardModel player in x)
+            {
+                playerList.Add(new tempPlayer
+                {
+                    playerName = player.user.username,
+                    highScore = player.score
+                });
+                
+                
+                
+            }
+
+            //Only generate list if leaderboard was successfully loaded.
+            GenerateList();
+        }
+        else //failed to load leaderboard;
+        
+        Debug.Log("Finished running GetLeaderboard request.");
+    }
+    
     private void GenerateList()
     {
         //set position and height
         RectTransform contentRect = GameObject.Find("Content").GetComponent<RectTransform>();
-        int top = currentPlayerIndex * 70;
-        int bottom = (playerList.Count - currentPlayerIndex) * -70;
-
-        contentRect.offsetMax = new Vector2(contentRect.offsetMax.x, top);
-        contentRect.offsetMin = new Vector2(contentRect.offsetMin.x, bottom);
+        contentRect.sizeDelta = new Vector2(contentRect.sizeDelta.x, Screen.width / 8 * playerList.Count);
+        contentRect.position = new Vector2(contentRect.position.x, -Screen.width / 8 * currentPlayerIndex);
         
 
         for (int i = 0; i < playerList.Count; i++)
@@ -76,25 +78,10 @@ public class LeaderboardTable : MonoBehaviour
             }
         }
     }
-    
-    private async void GetLeaderboard()
-    {
-        //Format WWWForm to acquire score data
-        WWWForm ScoresRequest = new WWWForm();
-        ScoresRequest.AddField("levelID", LeaderboardManager.LevelID);
 
-        string requestResult = await BackendManager.GETRequest("https://grana.vinniehat.com/api/auth/signin");
-        if (requestResult != null)
-        {
-            //Deserialize score data and convert to List<tempPlayer>
-            /*var x = JsonConvert.DeserializeObject<SignInModel>(requestResult);
-            PlayerPrefs.SetString("accessToken", x.accessToken);
-            Debug.Log("accessToken: " + x.accessToken);*/
-            playerList = new List<tempPlayer>();
-        }
-        else //failed to load leaderboard;
+    private void GetPlayerIndex()
+    {
         
-        Debug.Log("Completed GetLeaderboard request.");
     }
     
     private class tempPlayer
